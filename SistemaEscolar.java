c2514dd3353940f02a0176ca1e9e87290cf13efd
Hellen Anellize <hellenanellize100405@gmail.com>
2026-04-25 11:57:54 -0300

    Fix typo in greeting message and remove unnecessary line

--- SistemaEscolar.java
+++ SistemaEscolar.java
@@ -1,5 +1,7 @@
 public class SistemaEscolar {
     public static void main(String[] args) {
      System.out.println("Ola bebesssss");
+     System.out.println("Bem vindos ao sistema escolar");
+     sout;
     }
 }
